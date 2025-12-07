c8e53e172f0b4ef679a4d2cf5c9829a8684ad41c
Hilman Fikry <hilman12fikry@gmail.com>
2025-12-07 21:29:08 +0700

    chore: set up the project

---
+++ app.ts
@@ -0,0 +1,11 @@
+import "dotenv/config";
+import express from "express";
+import path from "node:path";
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
