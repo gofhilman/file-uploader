4318d2529a8d7da0c1066a9bf4d10ddb67f4a992
Hilman Fikry <hilman12fikry@gmail.com>
2025-12-09 11:33:17 +0700

    feat: create indexRouter paths

--- routes/indexRouter.ts
+++ routes/indexRouter.ts
@@ -2,4 +2,20 @@ import { Router } from "express";
 
 const indexRouter = Router();
 
+indexRouter.get("/");
+indexRouter.get("/:folderId");
+indexRouter.get("/:fileId/download");
+indexRouter.get("/login");
+indexRouter.get("/signup");
+
+indexRouter.post("/:folderId/create-folder");
+indexRouter.post("/:folderId/upload-file");
+indexRouter.post("/:folderId/rename");
+indexRouter.post("/:folderId/delete");
+indexRouter.post("/:folderId/share");
+indexRouter.post("/:fileId/rename");
+indexRouter.post("/:fileId/delete");
+indexRouter.post("/login");
+indexRouter.post("/signup");
+
 export default indexRouter;
